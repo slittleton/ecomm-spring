5098d8fc910e8c008f7d654664f45cac39673685
slittleton <littletonltp@gmail.com>
2022-02-17 12:02:23 -0700

    commit

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -14,45 +15,61 @@ import com.shopme.common.entity.User;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping("/users")
 	public String listAll(Model model) {
-		
+
 		List<User> listUsers = service.listAll();
 		model.addAttribute("listUsers", listUsers);
-		
-		
+
 		return "users";
-		
+
 	}
-	
+
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
 		List<Role> listRoles = service.listRoles();
 		User user = new User();
-		
+
 		user.setEnabled(true);
-		
+
 		model.addAttribute("user", user);
 		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Create New User");
 		
 		return "user_form";
 	}
-	
+
 	@PostMapping("/users/save")
 	public String saveUser(User user, RedirectAttributes redirectAttributes) {
-		
-		
+
 		System.out.println(user.toString());
-		
+
 		service.save(user);
-		
+
 		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
-		
+
 		return "redirect:/users";
 	}
-	
+
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes redirectAttributes)
+			throws UserNotFoundException {
+
+		try {
+			User user = service.get(id);
+			model.addAttribute("user", user);
+			model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+			
+			return "user_form";
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+			return "redirect/users";
+		}
+
+	}
+
 }
